669868d58ef8e7e5140885d7597cec12a56db8a3
Raj Nath Patel <patelrajnath@gmail.com>
2020-07-06 00:44:17 +0530

    'fix'

--- preprocess.py
+++ preprocess.py
@@ -5,7 +5,7 @@ train_data = list()
 import pandas as pd
 
 # train = pd.read_csv('data/test.csv')
-train = pd.read_csv('data/test.csv')
+train = pd.read_csv('data/train.csv')
 for id, row in train.iterrows():
     template = {
         'context': "",
@@ -27,14 +27,14 @@ for id, row in train.iterrows():
     # print(row['selected_text'])
     # print(row['sentiment'])
     # print(row['textID'])
-    template['context'] = row['text']
+    template['context'] = str(row['text'])
     template['qas'][0]['id'] = row['textID']
     template['qas'][0]['question'] = row['sentiment']
-    # template['qas'][0]['answers'][0]['text'] = row['selected_text']
-    # try:
-    #     template['qas'][0]['answers'][0]['answer_start'] = row['text'].index(row['selected_text'])
-    # except AttributeError:
-    #     print(id, row['text'], row['selected_text'])
+    template['qas'][0]['answers'][0]['text'] = row['selected_text']
+    try:
+        template['qas'][0]['answers'][0]['answer_start'] = row['text'].index(row['selected_text'])
+    except AttributeError:
+        print(id, row['text'], row['selected_text'])
 
     train_data.append(template)
     # template = None
@@ -44,5 +44,5 @@ for id, row in train.iterrows():
     #     break
     # print(train_data)
 # print(train_data)
-with open('data/test_processed.json', 'w') as f:
+with open('data/train_processed.json', 'w') as f:
     json.dump(train_data, f)
