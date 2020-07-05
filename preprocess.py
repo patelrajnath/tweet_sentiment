14ff2314ad50088e7608484bf5b12f54a73362a7
Raj Nath Patel <patelrajnath@gmail.com>
2020-07-06 00:38:46 +0530

    Fix

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,13 @@
 import json
 
-template = {
+train_data = list()
+
+import pandas as pd
+
+# train = pd.read_csv('data/test.csv')
+train = pd.read_csv('data/test.csv')
+for id, row in train.iterrows():
+    template = {
         'context': "",
         'qas': [
             {
@@ -16,13 +23,6 @@ template = {
             }
         ]
     }
-
-train_data = []
-
-import pandas as pd
-
-train = pd.read_csv('data/test.csv')
-for id, row in train.iterrows():
     # print(row['text'])
     # print(row['selected_text'])
     # print(row['sentiment'])
@@ -34,13 +34,15 @@ for id, row in train.iterrows():
     # try:
     #     template['qas'][0]['answers'][0]['answer_start'] = row['text'].index(row['selected_text'])
     # except AttributeError:
-        # print(id, row['text'], row['selected_text'])
+    #     print(id, row['text'], row['selected_text'])
 
     train_data.append(template)
+    # template = None
+    # print(id, train_data)
     # print(template)
-    # if id == 10:
-    #     exit()
+    # if id == 2:
+    #     break
     # print(train_data)
-print(train_data)
+# print(train_data)
 with open('data/test_processed.json', 'w') as f:
     json.dump(train_data, f)
